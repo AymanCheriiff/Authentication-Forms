33bfffeaf8933cb797e7f60e235b0c1d2db8e95a
Ayman Cherif <gdouralobna0@gmail.com>
2024-09-07 11:19:48 +0100

    update

--- authentication/src/app/app.module.ts
+++ authentication/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
